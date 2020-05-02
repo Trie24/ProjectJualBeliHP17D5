6cb5d8ef0aa4c7eef18a61f6ccb69105e486c08c
Tri Purwanto <Tpurwanto12@gmail.com>
2020-05-03 02:35:16 +0700

    Database API Project Jual Beli HP 17D5

---
+++ delete_phone.php
@@ -0,0 +1,10 @@
+<?php
+include'koneksi.php';
+
+$id = isset($_POST ['id']) ? $_POST['id'] : '';
+$query = 'DELETE from tbl_phone where id = '.$id;
+$result = mysqli_query($koneksi,$query) or die();
+if (mysql_affected_rows()>0) {
+	echo 'Delete Data Success';
+} else { echo'';}
+?>
---
+++ index.php
@@ -0,0 +1,8 @@
+<html>
+<head>
+	<title>Index Harga Handphone</title>
+</head>
+<body>
+	<h3>Selamat Datang Di Website Harga HP</h3>
+</body>
+</html>
