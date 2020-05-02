203c9bf5af9cbb2adbf32e9057cb39ca51b87546
Tri Purwanto <Tpurwanto12@gmail.com>
2020-05-03 02:31:34 +0700

    UTS Jual Beli HP Tri Purwanto 311710614

--- JualBeliHp17D5/app/src/main/java/com/smart/jualbelihp17d5/server/AsyncInvokeURLTask.java
+++ JualBeliHp17D5/app/src/main/java/com/smart/jualbelihp17d5/server/AsyncInvokeURLTask.java
@@ -20,14 +20,14 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public class AsyncInvokeURLTask extends AsyncTask<Void, Void, String> {
-    public String mNoteItWebUrl = "www.smartneasy.com";
+    public String mNoteItWebUrl = "http://192.168.43.17/xphone/";
     private ArrayList<NameValuePair> mParams;
     private OnPostExecuteListener mPostExecuteListener = null;
     private ProgressDialog dialog;
     public boolean showdialog =false;
     public String message ="Proses Data";
     //Harus diganti dengan IP Server yang digunakan
-    public String url_server ="http://192.168.43.125/xphone/";
+    public String url_server ="https://siakad-altie.000webhostapp.com/xphone/";
     public Context applicationContext;
     public static interface OnPostExecuteListener{
         void onPostExecute(String result);
